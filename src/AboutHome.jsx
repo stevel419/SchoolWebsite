04136a91b2d896a3453eae96759ea60647dbe3f0
stevel419 <ylin459@wisc.edu>
2025-06-05 12:10:18 +0300

    Completed Offer Section

---
+++ src/AboutHome.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+function AboutHome() {
+    return (
+        <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 bg-white place-content-center px-8 py-8 shadow-xl">
+            <div className="lg:col-span-2 col-span-1 p-4 text-black text-center">
+                <h2 className="font-sans text-2xl font-bold text-black uppercase">About Kiguruyembe Secondari</h2>
+                <p>
+                Kigurunyembe Secondary School, located in Morogoro, owned by the Catholic Diocese of Morogoro, 
+                Tanzania, traces its roots back to 1974. The inception of the school was inspired by the late 
+                Fr. J. Franken, who at the time was the Principal of Kigurunyembe Teachers College, currently known 
+                as Morogoro Teachers College, Fr Norbert Mpande who is currently working at the Cathedral.
+                </p>
+                <Link to="/about" className="text-blue-700 font-semibold hover:underline hover:text-blue-500">LEARN MORE →</Link>
+            </div>
+            <div className="p-4 rounded-xl border-2 border-black bg-emerald-700/50 text-black">
+                <h2 className="font-sans text-2xl font-bold text-black uppercase">Our Mission</h2>
+                <p>
+                Kigurunyembe secondary school is a church-based school that intends to provide high quality
+                education that produces active, flexible responsible and productive individuals in their daily life
+                situations.
+                </p>
+            </div>
+            <div className="p-4 rounded-xl border-2 border-black bg-emerald-700/50 text-black">
+                <h2 className="font-sans text-2xl font-bold text-black uppercase">Our Vision</h2>
+                <p>
+                Kigurunyembe secondary school will stand at the highest position in giving high quality education
+                that enables its scholars to become effective and functional citizens spiritually, economic and social.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default AboutHome;
--- src/Offer.jsx
+++ src/Offer.jsx
@@ -1,16 +1,78 @@
-function Offer() {
+import { useState } from "react";
+
+const OfferContents1 = () => {
     return (
-        <div className="Offer">
-            <h2 className="flex justify-center py-6">What we offer</h2>
-            <div className="flex flex-col lg:flex-row">
-                <div>
-                    <p>Holistic Formation (Mind, Body, and Soul)</p>
+        <ul className="list-inside list-disc py-2">
+            <li className="pb-2">
+                Develops not just intellectual abilities but also character, virtues, and a sense of purpose.
+            </li>
+            <li>
+                Encourages students to see their education as a means to serve God and humanity.
+            </li>
+        </ul>
+    );
+}
+
+const OfferContents2 = () => {
+    return (
+        <ul className="list-inside list-disc py-2">
+            <li className="pb-2">
+                Maintains a structured environment that emphasizes self-discipline, respect for teachers, and
+                obedience to God.
+            </li>
+            <li>
+                An alternative to schools where discipline is lacking.
+            </li>
+        </ul>
+    );
+}
+
+const OfferContents3 = () => {
+    return (
+        <ul className="list-inside list-disc py-2">
+            <li className="pb-2">
+                Provides a protective space against negative influences (e.g., substance abuse, violence, or
+                radical ideologies).
+            </li>
+            <li>
+                Teachers and staff act as role models in faith and virtue.
+            </li>
+        </ul>
+    );
+}
+
+function Offer() {
+    const [openMenu1, setOpenMenu1] = useState(false);
+    const [openMenu2, setOpenMenu2] = useState(false);
+    const [openMenu3, setOpenMenu3] = useState(false);
+
+    return (
+        <div className="bg-emerald-700/75">
+            <h2 className="flex justify-center pt-10 font-bold text-2xl uppercase">What we offer</h2>
+            <div className="flex flex-col lg:flex-row gap-10 px-10 py-10 lg:items-start">
+                <div className="border-y-2 p-4 rounded-xl bg-white flex-1">
+                    <button onClick={() => setOpenMenu1((prev) => !prev)} 
+                    className="flex justify-between items-center w-full text-left font-bold text-emerald-700">
+                        <span>Holistic Formation (Mind, Body, and Soul)</span>
+                        <span className="text-right text-xl">{ openMenu1 ? '-' : '+' }</span>
+                    </button>
+                    { openMenu1 && <OfferContents1 /> }
                 </div>
-                <div>
-                    <p>Discipline and Respect for Authority</p>
+                <div className="border-y-2 p-4 rounded-xl bg-white flex-1">
+                    <button onClick={() => setOpenMenu2((prev) => !prev)}
+                    className="flex justify-between items-center w-full text-left font-bold text-emerald-700">
+                        <span>Discipline and Respect for Authority</span>
+                        <span className="text-right text-xl">{ openMenu2 ? '-' : '+' }</span>
+                    </button>
+                    { openMenu2 && <OfferContents2 /> }
                 </div>
-                <div>
-                    <p>Safe and Nurturing Environment</p>
+                <div className="border-y-2 p-4 rounded-xl bg-white flex-1">
+                    <button onClick={() => setOpenMenu3((prev) => !prev)}
+                    className="flex justify-between items-center w-full text-left font-bold text-emerald-700">
+                        <span>Safe and Nurturing Environment</span>
+                        <span className="text-right text-xl">{ openMenu3 ? '-' : '+' }</span>
+                    </button>
+                    { openMenu3 && <OfferContents3 /> }
                 </div>
             </div>
         </div>
