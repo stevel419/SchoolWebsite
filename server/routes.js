124b7b55ec19f244901a6155a9e934acc511a15c
stevel419 <ylin459@wisc.edu>
2025-06-10 14:09:49 +0300

    Added Schemas

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { Teacher, Student, Grade, Attendance, Comment } = require('./schemas.js');
 
 router.get('/test', (req, res) => {
     res.json({ message: 'test, delete!' });
