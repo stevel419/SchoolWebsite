cdc9a36c10b9cf395a1793cd94924ed33365ad70
stevel419 <ylin459@wisc.edu>
2025-06-10 10:01:14 +0300

    Routes Setup

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
-const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+const routes = require('./routes');
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-try {
-    mongoose.connect(process.env.MONGO_URI);
-    console.log("Successfully connected to MongoDB");
-} catch (e) {
-    console.log(e);
-}
+connectDB();
+
+app.use(express.json());
+
+app.use('/api', routes);
 
 app.listen(port, () => {
     console.log("Server running on port " + port);
-})
+});
