9e22cc32e113425408274d132f363d67c7273aba
stevel419 <ylin459@wisc.edu>
2025-07-15 23:26:03 +0300

    Image fix

--- client/src/components/Slideshow.jsx
+++ client/src/components/Slideshow.jsx
@@ -50,7 +50,7 @@ const Slideshow = () => {
                 <img
                   src={encodeURI(slide.image)}
                   alt={`Slide ${index + 1}`}
-                  className="max-w-full max-h-full object-cover object-center"
+                  className="max-w-full max-h-full object-cover"
                 />
               </div>
   
