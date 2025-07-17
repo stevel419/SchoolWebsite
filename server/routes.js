5d6713ceec922ff96a26fbdaa445c84910ca8e82
stevel419 <ylin459@wisc.edu>
2025-07-18 01:26:42 +0300

    Increased file size limit for update website

--- server/routes.js
+++ server/routes.js
@@ -645,7 +645,7 @@ const upload = multer({
     }
   }),
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB limit
+    fileSize: 20 * 1024 * 1024, // 10MB limit
   },
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith('image/')) {
